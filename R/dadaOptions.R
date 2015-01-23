5e6900af40bab2713020d1df9a6704f06e01227e
benjjneb <benjamin.j.callahan@gmail.com>
2015-01-23 15:38:37 -0800

    Added facility to set/get DADA options
    There are a number of options that can be passed to the DADA algorithm.
    set_dada_opt and get_dada_opt allow those options to be viewed/changed.
    
    There is currently no consistency checking when setting options, this
    would probably be good to add when closer to a finished product.

---
+++ R/dadaOptions.R
@@ -0,0 +1,81 @@
+
+dada_opts <- new.env()
+assign("OMEGA_A", 0.01, envir = dada_opts)
+assign("HOMOPOLYMER_GAPPING", FALSE, envir = dada_opts)
+assign("SCORE_MATRIX", matrix(c(5, -4, -4, -4, -4, 5, -4, -4, -4, -4, 5, -4, -4, -4, -4, 5), nrow=4, byrow=TRUE), envir = dada_opts)
+assign("USE_KMERS", TRUE, envir = dada_opts)
+assign("KDIST_CUTOFF", 0.5, envir = dada_opts)
+assign("BAND_SIZE", 50, envir = dada_opts)
+assign("GAP_PENALTY", -8, envir = dada_opts)
+
+################################################################################
+#' Set DADA options
+#'
+#' @param option (Required). Character.
+#'  The DADA options to set.
+#' 
+#' @param value (Required). Corresponding variables of the appropriate data type.
+#'  option and value must have the same length.
+#'  WARNING: DATA TYPE CONSISTENCY IS NOT CURRENTLY CHECKED!!
+#' 
+#' @export
+#'
+set_dada_opt <- function(option, value) {
+  if(!all(option %in% ls(dada_opts))) {
+    stop(paste("Invalid DADA option:", option))
+  }
+  
+  if(length(option) != length(value)) {
+    stop(paste("Different lengths of name/value vectors:", length(option), ",", length(value)))
+  }
+  
+  for(i in seq(length(option))) {
+    assign(option[[i]], value[[i]], envir=dada_opts)
+  }
+}
+
+################################################################################
+#' Get DADA options
+#'
+#' @param option (Optional). Character.
+#'  The DADA options to get.
+#' 
+#' @return Named list of option/value pairs.
+#'  Returns NULL if an invalid option is requested.
+#' 
+#' @export
+#'
+get_dada_opt <- function(option = NULL) {
+  if(is.null(option)) option <- ls(dada_opts)
+  
+  if(!all(option %in% ls(dada_opts))) {
+    warning("Tried to get an invalid DADA option:", option[!(option %in% ls(dada_opts))])
+    option <- option[option %in% ls(dada_opts)]
+  }
+
+  ropts <- lapply(option, function(x) get(x, envir=dada_opts))
+  names(ropts) <- option
+  return(ropts)
+}
+
+
+
+#define VERBOSE 0
+#define tVERBOSE 0
+#define BUFFER_SIZE 1250
+#define GAPPEN -8
+#define KEY_BUFSIZE 2000
+#define SEQLEN 900 // Buffer size for DNA sequences read in from uniques files
+#define HASHOCC 20
+#define BAND 50 // Size of band in banded alignments. 0 means no banding.
+#define USE_KMERS 1
+#define KMER_SIZE 6
+#define KDIST_CUTOFF 0.5
+#define RAWBUF 50
+#define FAMBUF 50
+#define CLUSTBUF 50
+#define TAIL_APPROX_CUTOFF 0.0000000001
+#define OMEGA_A 0.01
+
+
+
