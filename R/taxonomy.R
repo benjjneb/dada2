caae8f30ca0bdb111c3ca401ada60c4199e56e35
benjjneb <benjamin.j.callahan@gmail.com>
2016-07-18 23:14:02 -0700

    Added assignSpecies functionality
    The assignSpecies function uses exact sequence matching to assign
    taxonomy down to the species level.

--- R/taxonomy.R
+++ R/taxonomy.R
@@ -85,3 +85,148 @@ assignTaxonomy <- function(seqs, refFasta, minBoot=50, verbose=FALSE) {
   tax.out[tax.out=="_DADA2_UNSPECIFIED"] <- NA_character_
   tax.out
 }
+
+# Helper function for assignSpecies
+mapHits <- function(x, refs, keep, sep="/") {
+  if(length(unique(refs[x]))<=keep) {
+    rval <- do.call(paste, c(as.list(sort(unique(refs[x]))), sep=sep))
+  } else { rval <- NA_character_ }
+  if(length(rval)==0) rval <- NA_character_
+  rval
+}
+
+#'
+#' Taxonomic assignment to the species level by exact matching.
+#' 
+#' assignSpecies uses exact matching against a reference fasta to identify the 
+#' genus-species binomial classification of the input sequences.
+#' 
+#' @param seqs (Required). A character vector of the sequences to be assigned, or an object 
+#' coercible by \code{\link{getUniques}}.
+#'   
+#' @param refFasta (Required). The path to the reference fasta file, or an 
+#' R connection Can be compresssed.
+#' This reference fasta file should be formatted so that the id lines correspond to the
+#' genus-species of the associated sequence:
+#' 
+#'  >SeqID genus species  
+#'  ACGAATGTGAAGTAA......
+#' 
+#' @param returnMultiple (Optional). Default FALSE.
+#' Defines the behavior when multiple exact matches against different species are returned.
+#' By default only unambiguous identifications are return. If TRUE, a concatenated string
+#' of all exactly matched species is returned. If an integer is provided, multiple
+#' identifications up to that many are returned as a concatenated string.
+#'   
+#' @param verbose (Optional). Default FALSE.
+#'  If TRUE, print status to standard output.
+#' 
+#' @return A two-column character matrix. Rows correspond to the provided sequences,
+#'   columns to the genus and species taxonomic levels. NA indicates that the sequence
+#'   was not classified at that level. 
+#' 
+#' @export
+#' 
+#' @importFrom ShortRead readFasta
+#' @importFrom ShortRead sread
+#' @importFrom ShortRead id
+#' 
+#' @examples
+#' \dontrun{
+#'  taxa <- assignSpecies(dadaF, "rdp_species.fa.gz")
+#' }
+#' 
+assignSpecies <- function(seqs, refFasta, returnMultiple=FALSE, verbose=FALSE) {
+  # Get character vector of sequences
+  seqs <- getSequences(seqs)
+  # Read in the reference fasta
+  refsr <- readFasta(refFasta)
+  refs <- as(sread(refsr), "character")
+  ids <- as(id(refsr), "character")
+  genus <- sapply(strsplit(ids, "\\s"), `[`, 2)
+  species <- sapply(strsplit(ids, "\\s"), `[`, 3)
+  # Identify the exact hits
+  hits <- lapply(seqs, function(x) grepl(x, refs, fixed=TRUE))
+  # Define number of multiple species to return
+  if(is.logical(returnMultiple)) {
+    if(returnMultiple) keep <- Inf
+    else keep <- 1
+  } else {
+    keep <- as.integer(returnMultiple)
+  }
+  # Get genus species return strings
+  rval <- cbind(unlist(sapply(hits, mapHits, refs=gens, keep=1)),
+                unlist(sapply(hits, mapHits, refs=species, keep=keep)))
+  colnames(rval) <- c("Genus", "Species")
+  rval
+}
+
+#' This function creates the dada2 assignSpecies fasta file for the RDP
+#' from the rdp_Bacteria_unaligned.fa file.
+#' @keywords internal
+makeSpeciesFasta_RDP <- function(fin, fout, compress=TRUE) {
+  # NOTE: How to deal with genera with groups (eg. Clostridium XII) is not
+  #  yet fully resolved.
+  # Read in and remove records not assigned to species
+  sr <- readFasta(fin)
+  is.uncult <- grepl("[Uu]ncultured", id(sr))
+  sr <- sr[!is.uncult]
+  is.unclass <- grepl("[Uu]nclassified", id(sr))
+  sr <- sr[!is.unclass]
+  is.outgroup <- (grepl("Outgroup", id(sr)))
+  sr <- sr[!is.outgroup]
+  is.unident <- grepl("[Uu]nidentified", id(sr))
+  sr <- sr[!is.unident]
+  
+  # Pull out the genus species binomial string
+  binom <- sapply(strsplit(as.character(id(sr)), ";"), `[`, 1)
+  binom <- sapply(strsplit(binom, "\\t"), `[`, 1)
+  binom <- gsub(" \\(T\\)", "", binom)
+  binom <- gsub("\\[", "", binom)
+  binom <- gsub("\\]", "", binom)
+  
+  # Match genera between binomial and the curated taxonomy
+  bar <- strsplit(as.character(id(sr)), ";")
+  barlens <- sapply(bar, length)
+  geni <- mapply(function(x,y) x[[y]], bar, barlens-1)
+  
+  # Get rid of SXXX id
+  binom <- gsub("^S[0123456789]{9} ", "", binom)
+  binom <- gsub("\'" , "", binom)
+  # Drop Candidatus strings
+  binom <- gsub("Candidatus ", "", binom)
+  geni <- gsub("Candidatus ", "", geni)
+  # Curated taxonomy has Clostridium groups (eg. Clostridium XI) but binomial names don't
+  geni[grepl("Clostridium", geni)] <- "Clostridium"
+  # Curated taxonomy has Escherichia/Shigella genus, but binomial names don't
+  binom <- gsub("^Escherichia ", "Escherichia/Shigella ", binom)
+  binom <- gsub("^Shigella ", "Escherichia/Shigella ", binom) 
+  ## Armatimonas/Armatimonadetes_gp1, Chthonomonas/Armatimonadetes_gp3
+  #### !!!! Still losing those two non-ecoli split genera
+  
+  # Subset down to those binomials which match the curated genus
+  gen.match <- mapply(grepl, paste0("^", geni), binom)
+  sr <- sr[gen.match]
+  binom <- binom[gen.match]
+  geni <- geni[gen.match]
+  
+  # Make matrix of genus/species
+  binom[sapply(strsplit(binom, "\\s"), length)==1] <- paste(binom[sapply(strsplit(binom, "\\s"), length)==1], "sp.")
+  binom2 <- cbind(sapply(strsplit(binom, "\\s"), `[`, 1),
+                sapply(strsplit(binom, "\\s"), `[`, 2))
+  # Keep only those with a named species
+  has.spec <- !grepl("sp\\.", binom2[,2])
+  sum(has.spec)
+  binom2 <- binom2[has.spec,]
+  sr <- sr[has.spec]
+  binom <- binom[has.spec]
+  geni <- geni[has.spec]
+  cat(length(binom), "sequences with genus/species binomial annotation found.\n")
+  
+  # Write to disk
+  ids <- as.character(narrow(id(sr),1,10))
+  writeFasta(ShortRead(sread(sr), BStringSet(paste(ids, binom))), fout,
+             width=20000L, compress=compress)
+  
+  # Done for now.
+}
