a68ce40ae352ad900af485c5c58ffa1fa9e7b6bd
Gene Blanchard <gblanchard4@gmail.com>
2016-09-30 14:19:33 -0500

    fixed occurrences in errorModels.R

--- R/errorModels.R
+++ R/errorModels.R
@@ -113,7 +113,7 @@ inflateErr <- function(err, inflation, inflateSelfTransitions = FALSE) {
 #
 # @param omegaB (Optional). A \code{numeric(1)}. Default is 1e-10.
 #  The p-value threshold below which a base is assigned as "bad".
-#  The p-value is calculated by the number of repeated occurences of a particular
+#  The p-value is calculated by the number of repeated occurrences of a particular
 #    base position individually driving the formation of a new cluster. Bad bases
 #    drive many new "1-away" clusters.
 #  The null hypothesis being tested is that real differences are distributed
