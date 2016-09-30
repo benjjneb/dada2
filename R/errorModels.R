4ad7c06bd8fe31a613130cc1410775fe0624ba6c
Gene Blanchard <gblanchard4@gmail.com>
2016-09-30 14:20:23 -0500

    fixed threshold in errorModels.R

--- R/errorModels.R
+++ R/errorModels.R
@@ -157,7 +157,7 @@ isBadBaseFP <- function(clust, birth_subs, minFraction = 0.51, omegaB = 1e-10, m
 # @param birth_subs (Required). The $birth_subs data frame from the dada() output.
 #
 # @param omegaB (Optional). A \code{numeric(1)}. Default is 1e-10.
-#  The p-value threshhold below which a base is assigned as "bad".
+#  The p-value threshold below which a base is assigned as "bad".
 #  The p-value is calculated by the number of repeated occurences of a particular
 #    base position individually driving the formation of a new cluster. Bad bases
 #    drive many new "1-away" clustes.
