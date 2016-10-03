f6d8ddb0a51820ad51e1efd9272470a98f158606
Gene Blanchard <gblanchard4@gmail.com>
2016-10-03 13:16:40 -0500

    fixed statistical, matches in dada.R

--- R/dada.R
+++ R/dada.R
@@ -19,83 +19,83 @@ assign("HOMOPOLYMER_GAP_PENALTY", NULL, envir = dada_opts)
 # assign("FINAL_CONSENSUS", FALSE, envir=dada_opts) # NON-FUNCTIONAL AT THE MOMENT
 
 #' High resolution sample inference from amplicon data.
-#'
+#' 
 #' The dada function takes as input dereplicated amplicon sequencing reads and returns the inferred composition
 #'  of the sample (or samples). Put another way, dada removes all sequencing errors to reveal the members of the
 #'  sequenced community.
-#'
+#'  
 #' If dada is run in selfConsist=TRUE mode, the algorithm will infer both the sample composition and
 #'  the parameters of its error model from the data.
-#'
+#'  
 #' @param derep (Required). A \code{\link{derep-class}} object, the output of \code{\link{derepFastq}}.
 #'  A list of such objects can be provided, in which case each will be denoised with a shared error model.
-#'
+#'  
 #' @param err (Required). 16xN numeric matrix. Each entry must be between 0 and 1.
-#'
+#' 
 #'  The matrix of estimated rates for each possible nucleotide transition (from sample nucleotide to read nucleotide).
-#'
+#'  
 #'  Rows correspond to the 16 possible transitions (t_ij) indexed such that 1:A->A, 2:A->C, ..., 16:T->T
-#'
+#'    
 #'  Columns correspond to quality scores. Typically there are 41 columns for the quality scores 0-40.
 #'  However, if USE_QUALS=FALSE, the matrix must have only one column.
-#'
+#'  
 #'  If selfConsist = TRUE, \code{err} can be set to NULL and an initial error matrix will be estimated from the data
 #'  by assuming that all reads are errors away from one true sequence.
-#'
+#'    
 #' @param errorEstimationFunction (Optional). Function. Default \code{\link{loessErrfun}}.
-#'
-#'  If USE_QUALS = TRUE, \code{errorEstimationFunction(dada()$trans_out)} is computed after sample inference,
+#' 
+#'  If USE_QUALS = TRUE, \code{errorEstimationFunction(dada()$trans_out)} is computed after sample inference, 
 #'    and the return value is used as the new estimate of the err matrix in $err_out.
-#'
+#'    
 #'  If USE_QUALS = FALSE, this argument is ignored, and transition rates are estimated by maximum likelihood (t_ij = n_ij/n_i).
-#'
+#'  
 #' @param selfConsist (Optional). \code{logical(1)}. Default FALSE.
-#'
-#'  If selfConsist = TRUE, the algorithm will alternate between sample inference and error rate estimation
+#' 
+#'  If selfConsist = TRUE, the algorithm will alternate between sample inference and error rate estimation 
 #'    until convergence. Error rate estimation is performed by \code{errorEstimationFunction}.
-#'
+#'    
 #'  If selfConsist=FALSE the algorithm performs one round of sample inference based on the provided \code{err} matrix.
-#'
+#'   
 #' @param pool (Optional). \code{logical(1)}. Default is FALSE.
-#'
+#' 
 #'  If pool = TRUE, the algorithm will pool together all samples prior to sample inference.
 #'  If pool = FALSE, sample inference is performed on each sample individually.
-#'
+#'  
 #'  This argument has no effect if only 1 sample is provided, and \code{pool} does not affect
 #'   error rates, which are always estimated from pooled observations across samples.
-#'
+#'   
 #' @param multithread (Optional). Default is FALSE.
-#'  If TRUE, multithreading is enabled and the number of available threads is automatically determined.
+#'  If TRUE, multithreading is enabled and the number of available threads is automatically determined.   
 #'  If an integer is provided, the number of threads to use is set by passing the argument on to
 #'  \code{\link{setThreadOptions}}.
-#'
+#'   
 #' @param ... (Optional). All dada_opts can be passed in as arguments to the dada() function.
-#'  See \code{\link{setDadaOpt}} for a full list and description of these options.
+#'  See \code{\link{setDadaOpt}} for a full list and description of these options. 
 #'
-#' @return A \code{\link{dada-class}} object or list of such objects if a list of dereps was provided.
-#'
+#' @return A \code{\link{dada-class}} object or list of such objects if a list of dereps was provided. 
+#'   
 #' @details
-#'
-#' Briefly, \code{dada} implements a statistical test for the notion that a specific sequence was seen too many times
+#' 
+#' Briefly, \code{dada} implements a statiscal test for the notion that a specific sequence was seen too many times
 #'  to have been caused by amplicon errors from currently inferred sample sequences. Overly-abundant
 #'  sequences are used as the seeds of new clusters of sequencing reads, and the final set of clusters
 #'  is taken to represent the denoised composition of the sample. A more detailed explanation of the algorithm
 #'  is found in two publications:
-#'
-#' \itemize{
+#' 
+#' \itemize{ 
 #'  \item{Callahan BJ, McMurdie PJ, Rosen MJ, Han AW, Johnson AJ, Holmes SP (2015). DADA2: High resolution sample inference from amplicon data. bioRxiv, 024034.}
 #'  \item{Rosen MJ, Callahan BJ, Fisher DS, Holmes SP (2012). Denoising PCR-amplified metagenome data. BMC bioinformatics, 13(1), 283.}
 #' }
-#'
+#'  
 #' \code{dada} depends on a parametric error model of substitutions. Thus the quality of its sample inference is affected
-#'  by the accuracy of the estimated error rates. \code{selfConsist} mode allows these error rates to be inferred
+#'  by the accuracy of the estimated error rates. \code{selfConsist} mode allows these error rates to be inferred 
 #'  from the data.
-#'
-#' All comparisons between sequences performed by \code{dada} depend on pairwise alignments. This step is the most
+#'  
+#' All comparisons between sequences performed by \code{dada} depend on pairwise alignments. This step is the most 
 #'  computationally intensive part of the algorithm, and two alignment heuristics have been implemented for speed:
 #'  A kmer-distance screen and banded Needleman-Wunsch alignmemt. See \code{\link{setDadaOpt}}.
-#'
-#' @seealso
+#'  
+#' @seealso 
 #'  \code{\link{derepFastq}}, \code{\link{setDadaOpt}}
 #'
 #' @importFrom RcppParallel RcppParallelLibs
@@ -113,10 +113,10 @@ assign("HOMOPOLYMER_GAP_PENALTY", NULL, envir = dada_opts)
 dada <- function(derep,
                  err,
                  errorEstimationFunction = loessErrfun,
-                 selfConsist = FALSE,
+                 selfConsist = FALSE, 
                  pool = FALSE,
                  multithread = FALSE, ...) {
-
+  
   call <- sys.call(1)
   # Read in default opts and then replace with any that were passed in to the function
   opts <- getDadaOpt()
@@ -128,12 +128,12 @@ dada <- function(derep,
       warning(opnm, " is not a valid DADA option.")
     }
   }
-
+  
   # If a single derep object, make into a length 1 list
   if(class(derep) == "derep") { derep <- list(derep) }
   if(!is.list.of(derep, "derep")) { stop("The derep argument must be a derep-class object or list of derep-class objects.") }
   if(opts$USE_QUALS && any(is.null(lapply(derep, function(x) x$quals)))) { stop("The input derep-class object(s) must include quals if USE_QUALS is TRUE.") }
-
+  
   # Validate derep object(s)
   for(i in seq_along(derep)) {
     if(!(is.integer(derep[[i]]$uniques))) {
@@ -169,18 +169,18 @@ dada <- function(derep,
   qmax <- ceiling(qmax) # Only getting averages from derep$quals
   if(qmax > 45) {
     if(qmax > 62) {
-      stop("drep$quals matrix has an invalid maximum Phred Quality Scores of ", qmax)
+      stop("drep$quals matrix has an invalid maximum Phred Quality Scores of ", qmax) 
     }
     warning("derep$quals matrix has Phred Quality Scores >45. For Illumina 1.8 or earlier, this is unexpected.")
   }
-
+  
   # Pool the derep objects if so indicated
   if(length(derep) <= 1) { pool <- FALSE }
   if(pool) { # Make derep a length 1 list of pooled derep object
     derep.in <- derep
     derep <- list(combineDereps2(derep))
   }
-
+  
   # Validate err matrix
   initializeErr <- FALSE
   if(class(err) == "dada") { err <- err$err_out }
@@ -197,7 +197,7 @@ dada <- function(derep,
       message("The supplied error matrix does not extend to maximum observed Quality Scores in derep (", qmax, ").
   Extending error rates by repeating the last column of the Error Matrix (column ", ncol(err), ").
   In selfConsist mode this should converge to the proper error rates, otherwise this may not be what you want.")
-      for (q in seq(ncol(err), qmax)) {
+      for (q in seq(ncol(err), qmax)) { 
         err <- cbind(err, err[1:16, q])
         colnames(err)[q+1] <- q
       }
@@ -205,24 +205,24 @@ dada <- function(derep,
   }
 
   # Might want to check for summed transitions from NT < 1 also.
-
+  
   # Validate errorEstimationFunction
   if(!opts$USE_QUALS) {
     if(!is.null(errorEstimationFunction)) message("The errorEstimationFunction argument is ignored when USE_QUALS is FALSE.")
     errorEstimationFunction = NULL  # NULL error function has different meaning depending on USE_QUALS
   } else {
-    if(is.null(errorEstimationFunction)) {
+    if(is.null(errorEstimationFunction)) { 
       if(selfConsist) {
         warning("Did not provide errorEstimationFunction for selfConsist mode, using the default loessErrfun.")
         errorEstimationFunction <- loessErrfun
       } else {
-        message("Did not provide errorEstimationFunction, no post-dada error rates ($err_out) will be estimated.")
+        message("Did not provide errorEstimationFunction, no post-dada error rates ($err_out) will be estimated.") 
       }
     } else {
       if(!is.function(errorEstimationFunction)) stop("Must provide a function for errorEstimationFunction.")
     }
   }
-
+  
   # Validate alignment parameters
   if(opts$GAP_PENALTY>0) opts$GAP_PENALTY = -opts$GAP_PENALTY
   if(is.null(opts$HOMOPOLYMER_GAP_PENALTY)) { # Set gap penalties equal
@@ -234,7 +234,7 @@ dada <- function(derep,
     opts$VECTORIZED_ALIGNMENT <- FALSE # No homopolymer gapping in vectorized aligner
   }
   if(opts$VECTORIZED_ALIGNMENT) {
-    if(length(unique(diag(opts$SCORE)))!=1 ||
+    if(length(unique(diag(opts$SCORE)))!=1 || 
            length(unique(opts$SCORE[upper.tri(opts$SCORE) | lower.tri(opts$SCORE)]))!=1) {
       message("The vectorized aligner requires that the score matrix reduces to match/mismatch. Turning off vectorization.")
       opts$VECTORIZED_ALIGNMENT=FALSE
@@ -244,7 +244,7 @@ dada <- function(derep,
     }
     if(opts$BAND_SIZE == 0) opts$VECTORIZED_ALIGNMENT=FALSE
   }
-
+  
   # Parse multithreading argument
   if(is.logical(multithread)) {
     if(multithread==TRUE) { RcppParallel::setThreadOptions(numThreads = "auto") }
@@ -289,13 +289,13 @@ dada <- function(derep,
           cat("   selfConsist step", nconsist, "\n")
         }
       }
-      res <- dada_uniques(names(derep[[i]]$uniques), unname(derep[[i]]$uniques),
+      res <- dada_uniques(names(derep[[i]]$uniques), unname(derep[[i]]$uniques), 
                           if(initializeErr) { matrix(1, nrow=16, ncol=max(41,qmax+1)) } else { err },
-                          qi,
+                          qi, 
                           opts[["SCORE_MATRIX"]], opts[["GAP_PENALTY"]],
                           opts[["USE_KMERS"]], opts[["KDIST_CUTOFF"]],
                           opts[["BAND_SIZE"]],
-                          opts[["OMEGA_A"]],
+                          opts[["OMEGA_A"]], 
                           if(initializeErr) { 1 } else { opts[["MAX_CLUST"]] },
                           opts[["MIN_FOLD"]], opts[["MIN_HAMMING"]],
                           opts[["USE_QUALS"]],
@@ -305,11 +305,11 @@ dada <- function(derep,
                           opts[["HOMOPOLYMER_GAP_PENALTY"]],
                           multithread,
                           opts[["VERBOSE"]])
-
+      
       # Augment the returns
       res$clustering$sequence <- as.character(res$clustering$sequence)
       # ... nothing here for now
-
+      
       # List the returns
       clustering[[i]] <- res$clustering
       clusterquals[[i]] <- t(res$clusterquals) # make sequences rows and positions columns
@@ -322,7 +322,7 @@ dada <- function(derep,
     }
     # Accumulate the sub matrix
     cur <- Reduce("+", trans) # The only thing that changes is err(trans), so this is sufficient
-
+    
     # Estimate the new error model (if applicable)
     if(opts$USE_QUALS) {
       if(is.null(errorEstimationFunction)) {
@@ -351,13 +351,13 @@ dada <- function(derep,
       if(!(nrow(err)==16)) stop("Error matrix returned by errorEstimationFunction does not have 16 rows.")
       if(!all(err>=0)) stop("Error matrix returned by errorEstimationFunction has entries <0.")
       if(!all(err<=1)) stop("Error matrix returned by errorEstimationFunction has entries >1.")
-      if(any(err==0)) warning("Error matrix returned by errorEstimationFunction has 0s in some entries.")
+      if(any(err==0)) warning("Error matrix returned by errorEstimationFunction has 0s in some entries.")      
     }
-
+    
     # Termination condition for selfConsist loop
     if((!selfConsist) || any(sapply(errs, identical, err)) || (nconsist >= opts$MAX_CONSIST)) {
       break
-    }
+    } 
     nconsist <- nconsist+1
   } # repeat
 
@@ -369,7 +369,7 @@ dada <- function(derep,
       cat("\nConvergence after ", nconsist, " rounds.\n")
     }
   }
-
+  
   # Construct return object
   # A single dada-class object if one derep object provided.
   # A list of dada-class objects if multiple derep objects provided.
@@ -391,7 +391,7 @@ dada <- function(derep,
       rval2[[i]]$err_in <- errs[[1]]
     }
     rval2[[i]]$err_out <- err           # maybe better as _final? Just the last one
-
+    
     # Store the call and the options that were used in the return object
     rval2[[i]]$opts <- opts
     rval2[[i]]$call <- call
@@ -420,7 +420,7 @@ dada <- function(derep,
       rval2[[i]]$quality <- rval2[[i]]$quality[keep,,drop=FALSE] # Not the qualities for this sample alone!
       # Prune birth_subs and remap its $clust column
       rval2[[i]]$birth_subs <- rval2[[i]]$birth_subs[keep[rval2[[i]]$birth_subs$clust],,drop=FALSE]
-      rval2[[i]]$birth_subs$clust <- newBi[rval2[[i]]$birth_subs$clust]
+      rval2[[i]]$birth_subs$clust <- newBi[rval2[[i]]$birth_subs$clust]      
       # Remap $map
       rval2[[i]]$map <- newBi[map[names(derep.in[[i]]$uniques)]]
       # Recalculate abundances (both $denoised and $clustering$abundance)
@@ -452,56 +452,56 @@ dada <- function(derep,
 #'  part of the dada(...) function call itself by including a DADA_OPTION_NAME=VALUE argument.
 #'
 #' @param ... (Required). The DADA options to set, along with their new value.
-#'
-#' @return NULL.
-#'
+#'  
+#' @return NULL.  
+#'  
 #' @details The various dada options...
-#'
+#' 
 #' OMEGA_A: This parameter sets the threshold for when DADA2 calls unique sequences significantly overabundant, and therefore creates a
 #'  new cluster with that sequence as the center. The default value is 1e-40, which is a conservative setting to avoid making false
 #'  positive inferences, but which comes at the cost of reducing the ability to identify some rare variants.
-#'
+#' 
 #' USE_QUALS: If TRUE, the dada(...) error model takes into account the consensus quality score of the dereplicated unique sequences.
 #'  If FALSE, quality scores are ignored. The default is TRUE, however if applying DADA2 to pyrosequenced data it is recommended to set
 #'  USE_QUALS to FALSE, as quality scores are not informative about substitution error rates in pyrosequencing.
-#'
+#' 
 #' USE_KMERS: If TRUE, a 5-mer distance screen is performed prior to performing each pairwise alignment, and if the 5mer-distance
 #'  is greater than KDIST_CUTOFF, no alignment is performed. TRUE by default.
-#'
+#' 
 #' KDIST_CUTOFF: The default value of 0.42 was chosen to screen pairs of sequences that differ by >10\%, and was
 #'  calibrated on Illumina sequenced 16S amplicon data. The assumption is that sequences that differ by such a large
 #'  amount cannot be linked by amplicon errors (i.e. if you sequence one, you won't get a read of other) and so
 #'  careful (and costly) alignment is unnecessary.
-#'
+#' 
 #' BAND_SIZE: When set, banded Needleman-Wunsch alignments are performed. Banding restricts the net cumulative number of insertion
 #'  of one sequence relative to the other. The default value of BAND_SIZE is 16. If DADA is applied to marker genes with high rates
 #'  of indels, such as the ITS region in fungi, the BAND_SIZE parameter should be increased. Setting BAND_SIZE to a negative number
 #'  turns off banding (i.e. full Needleman-Wunsch).
-#'
+#' 
 #' SCORE_MATRIX: The score matrix for the Needleman-Wunsch alignment. This is a 4x4 matrix as no ambiguous nucleotides
-#'  are allowed. Default is nuc44: -4 for mismatches, +5 for matches.
-#'
+#'  are allowed. Default is nuc44: -4 for mismatches, +5 for matchces.
+#'  
 #' GAP_PENALTY: The cost of gaps in the Needleman-Wunsch alignment. Default is -8.
-#'
+#'  
 #' HOMOPOLYMER_GAP_PENALTY: The cost of gaps in homopolymer regions (>=3 repeated bases). Default is NULL, which causes homopolymer
-#'  gaps to be treated as normal gaps.
-#'
+#'  gaps to be treated as normal gaps.  
+#'  
 #' MIN_FOLD: The minimum fold-overabundance for sequences to form new clusters. Default value is 1, which means this
 #'  criteria is ignored.
-#'
+#'  
 #' MIN_HAMMING: The minimum hamming-separation for sequences to form new clusters. Default value is 1, which means this
 #'  criteria is ignored.
 #'
 #' MAX_CLUST: The maximum number of clusters. Once this many clusters have been created, the algorithm terminates regardless
 #'  of whether the statistical model suggests more sample sequences exist. If set to 0 this argument is ignored. Default
 #'  value is 0.
-#'
+#'  
 #' MAX_CONSIST: The maximum number of steps when selfConsist=TRUE. If convergence is not reached in MAX_CONSIST steps,
 #'  the algorithm will terminate with a warning message. Default value is 10.
-#'
+#'  
 #' VERBOSE: If TRUE progress messages from the algorithm are printed. Warning: There is a lot of output. Default is FALSE.
-#'
-#' @seealso
+#' 
+#' @seealso 
 #'  \code{\link{getDadaOpt}}
 #'
 #' @export
@@ -509,14 +509,14 @@ dada <- function(derep,
 #' @examples
 #' setDadaOpt(OMEGA_A = 1e-20)
 #' setDadaOpt(OMEGA_A = 1e-20, VERBOSE = TRUE)
-#'
+#'   
 setDadaOpt <- function(...) {
   opts <- getDadaOpt()
   args <- list(...)
   for(opnm in names(args)) {
     if(opnm %in% names(opts)) {
       if(class(getDadaOpt(opnm)) != class(args[[opnm]])) {
-        warning(paste0(opnm, " not set, value provided has different class (", class(args[[opnm]]),
+        warning(paste0(opnm, " not set, value provided has different class (", class(args[[opnm]]), 
                     ") then current option value (", class(getDadaOpt(opnm)), ")"))
       } else {
         assign(opnm, args[[opnm]], envir=dada_opts)
@@ -532,7 +532,7 @@ setDadaOpt <- function(...) {
 #  if(!(is.numeric(score) && dim(err) == c(4,4))) {
 #    stop("dada: Invalid score matrix.")
 #  }
-#
+#  
 #  if(!(is.numeric(gap_penalty) && gap_penalty <=0)) {
 #    stop("dada: Invalid gap penalty.")
 #  }
@@ -543,11 +543,11 @@ setDadaOpt <- function(...) {
 #'
 #' @param option (Optional). Character.
 #'  The DADA option(s) to get.
-#'
+#' 
 #' @return Named list of option/value pairs.
 #'  Returns NULL if an invalid option is requested.
-#'
-#' @seealso
+#' 
+#' @seealso 
 #'  \code{\link{setDadaOpt}}
 #'
 #' @export
@@ -555,15 +555,15 @@ setDadaOpt <- function(...) {
 #' @examples
 #' getDadaOpt("BAND_SIZE")
 #' getDadaOpt()
-#'
+#' 
 getDadaOpt <- function(option = NULL) {
   if(is.null(option)) option <- ls(dada_opts)
-
+  
   if(!all(option %in% ls(dada_opts))) {
     warning("Tried to get an invalid DADA option: ", option[!(option %in% ls(dada_opts))])
     option <- option[option %in% ls(dada_opts)]
   }
-
+  
   ropts <- lapply(option, function(x) get(x, envir=dada_opts))
   names(ropts) <- option
   if(length(ropts) == 1) ropts <- ropts[[1]]  # If just one option requested, return it alone
