7dcb444b8681370089c68d48d14dc925bb57d04c
benjjneb <benjamin.j.callahan@gmail.com>
2016-10-06 10:42:01 -0700

    Stop identical input/output files

--- R/filter.R
+++ R/filter.R
@@ -9,9 +9,9 @@
 #' (http://www.drive5.com/usearch/manual/cmd_fastq_filter.html). It adds the ability to remove
 #' contaminating phiX sequences as part of the filtering process.
 #' 
-#' @param fn (Required). The path to the input fastq file, or an R connection to that file.
+#' @param fn (Required). The path to the input fastq file.
 #'   
-#' @param fout (Required). The path to the output file, or an R connection to that file.
+#' @param fout (Required). The path to the output file.
 #'  Note that by default (\code{compress=TRUE}) the output fastq file is gzipped.
 #' 
 #' @param truncQ (Optional). Default 2.
@@ -88,6 +88,8 @@ fastqFilter <- function(fn, fout, truncQ = 2, truncLen = 0, trimLeft = 0, maxN =
   start <- max(1, trimLeft + 1)
   end <- truncLen
   if(end < start) { end = NA }
+  
+  if(fn == fout) { stop("The output and input files must be different.") }
   
   ## iterating over an entire file using fastq streaming
   f <- FastqStreamer(fn, n = n)
@@ -275,6 +277,8 @@ fastqPairedFilter <- function(fn, fout, maxN = c(0,0), truncQ = c(2,2), truncLen
   # Warning: This assumes that forward/reverse reads are in the same order unless matchIDs=TRUE
   if(!is.character(fn) || length(fn) != 2) stop("Two paired input file names required.")
   if(!is.character(fout) || length(fout) != 2) stop("Two paired output file names required.")
+
+  if(any(duplicated(c(fn, fout)))) { stop("The output and input file names must be different.") }
   
   for(var in c("maxN", "truncQ", "truncLen", "trimLeft", "minQ", "maxEE", "rm.phix")) {
     if(length(get(var)) == 1) { # Double the 1 value to be the same for F and R
